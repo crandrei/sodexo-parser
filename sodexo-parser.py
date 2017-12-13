a90f7677a50ea46a10eaa130b1f91c8c1fc9fc0f
Cristian Andrei <cristian.andrei@finnair.com>
2017-12-13 12:27:16 +0200

    Added language support for menu

--- sodexo-parser.py
+++ sodexo-parser.py
@@ -7,14 +7,21 @@ RESTAURANT_ID = 878
 
 date = time.strftime("%Y/%m/%d")
 
-response = requests.get('http://www.sodexo.fi/ruokalistat/output/daily_json/' + str(RESTAURANT_ID) + '/' + date + '/fi')
+language = sys.argv.pop()
+if language == 'sodexo-parser.py' :
+	# No language argument provided, default to Finnish
+	language = 'fi'
+
+title = 'title_' + language
+
+response = requests.get('http://www.sodexo.fi/ruokalistat/output/daily_json/' + str(RESTAURANT_ID) + '/' + date + '/' + language)
 json_data = json.loads(response.text)
 
 for entry in json_data['courses']:
 
     # There might be other than menu rows, skip them
-    if 'category' in entry and 'title_fi' in entry:
-        dish = colored('{0: <15}'.format(entry['category']), 'yellow') + entry['title_fi']
+    if 'category' in entry and title in entry:
+        dish = colored('{0: <15}'.format(entry['category']), 'yellow') + entry[title]
         if 'price' in entry:
             dish += colored(' (' + entry['price'] + ')', 'red')
         print dish
